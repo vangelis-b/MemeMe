62df14c7f3f364823becfeb140ec8336f8caa9df
Vangelis Bouzoukas <vangelis.b@icloud.com>
2020-04-11 23:49:45 -0700

    style: update camera/photo library access messages

---
+++ MemeMe/Models/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Vangelis Bouzoukas on 4/11/20.
+//  Copyright © 2020 Vangelis Bouzoukas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String = "TOP"
+    let bottomText: String = "BOTTOM"
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
